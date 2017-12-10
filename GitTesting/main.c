f71edf8eff68429c57a73cc38f043429969ac818
Suza Tripunovic <Suza Tripunovic@DESKTOP-6I9R7LE>
2017-12-10 23:02:06 +0100

    Funkcija faktorijel.

--- GitTesting/main.c
+++ GitTesting/main.c
@@ -19,9 +19,21 @@ void g(const int x, const int y)
 	return x * y;
 }
 
+int faktorijel(int x)
+{
+	int i,p=1;
+	for (i = ;i <= x;i++)
+		p *= i;
+	return p;
+
+}
 int main()
 {
-	int a = 2, b = 4;
+	int a = 2, b = 4,x;
+	printf("Unesi broj:");
+	scanf("%d", &x);
+	int p = faktorijel(x);
+	printf("Faktorijel broja %d je %d!", x, p);
 	printf("a="); scanf_s("%d", &a);
 	printf("b="); scanf_s("%d", &b);
 	printf("2`4= %d", Stepen(a, b));
