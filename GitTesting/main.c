3a7ed7aa4ffd5aec67f3d47707a769bff4421178
Amir Temimović <temimovica@gmail.com>
2017-12-10 23:52:15 +0100

    dodana funkcija za racunanje sume cifara

--- GitTesting/main.c
+++ GitTesting/main.c
@@ -14,7 +14,7 @@ void f()
 	printf("\nOvo nije normalno!\n");
 }
 
-void g(const int x, const int y)
+int g(const int x, const int y)
 {
 	return x * y;
 }
@@ -22,24 +22,36 @@ void g(const int x, const int y)
 int faktorijel(int x)
 {
 	int i,p=1;
-	for (i = ;i <= x;i++)
+	for (i =0 ;i <= x;i++)
 		p *= i;
 	return p;
 
+}
+
+int sumOfDigits(int x)
+{
+	int s = 0;
+	while (x)
+	{
+		s += x % 10;
+		x /= 10;
+	}
+	return s;
 }
 int main()
 {
 	int a = 2, b = 4,y;
 	printf("Unesi broj:");
 	scanf("%d", &y);
-	int p = faktorijel(x);
+	int p = faktorijel(y);
+	printf("Suma cifara broja %d je %d.\n", y, sumOfDigits(y));
 	printf("Faktorijel broja %d je %d!", y, p);
 	printf("a="); scanf_s("%d", &a);
 	printf("b="); scanf_s("%d", &b);
 	printf("2`4= %d", Stepen(a, b));
 	f();
 	int c = g(5, 6);
-	prinf("%d", c);
+	printf("%d", c);
 	getchar();
 	getchar();
 	return 0;
