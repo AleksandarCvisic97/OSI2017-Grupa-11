4f736b57e9b9c992361037cb283e1457654ab7ce
Grbić <dragan97grbic@hotmail.com>
2017-12-26 18:09:07 +0100

    Improvizovana stabilnost programa.

--- Main.cpp
+++ Main.cpp
@@ -4,18 +4,18 @@
 
 int main()
 {
-	int p = 0;
+	char c;
 	Admin fake("Petar", "Petrovic", "pero", "1234", 1);
 	do
 	{
 		print_main_menu();
-		std::cin >> p;
-		switch (p)
+		std::cin >> c;
+		switch (c)
 		{
-		case 1: fake.LogIn(); if (fake.get_code() == 0) print_admin_menu(); else print_analyst_menu(); break;
-		case 2: fake.PlaceRequest(); break;
-		case 3: std::cout << "Dovidjenja!" << std::endl; return 0; break;
-		case 4: print_request_menu(); fake.ApproveRegRequest(); break;
+		case '1': fake.LogIn(); if (fake.get_code() == 0) print_admin_menu(); else print_analyst_menu(); break;
+		case '2': fake.PlaceRequest(); break;
+		case '3': std::cout << "Dovidjenja!" << std::endl; return 0; break;
+		case '4': print_request_menu(); fake.ApproveRegRequest(); break;
 		default: std::cout << "Nepostojeca opcija! Pokusajte ponovo" << std::endl; break;
 		}
 	} while (true);
